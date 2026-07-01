be549b4bf27c080f27372a59a9b59a4e1ca6dc0d
Sijawusz Pur Rahnama <sija@sija.pl>
2026-07-01 12:24:36 +0200

    Linkify contributors + add separate const for rules reference url

--- src/consts.ts
+++ src/consts.ts
@@ -8,7 +8,9 @@ export const BLOG_DESCRIPTION = 'Release announcements and articles about the Am
 export const GITHUB_URL = 'https://github.com/crystal-ameba/ameba';
 export const GITHUB_RELEASES_URL = `${GITHUB_URL}/releases`;
 export const CONTRIBUTING_URL = `${GITHUB_URL}/blob/master/CONTRIBUTING.md`;
+export const CONTRIBUTORS_URL = 'https://github.com/crystal-ameba/ameba/graphs/contributors?all=1';
 export const DOCS_URL = 'https://crystal-ameba.github.io/ameba/';
+export const RULES_REFERENCE_URL = DOCS_URL; // TODO
 
 export const EDIT_BASE_URL =
   'https://github.com/crystal-ameba/crystal-ameba.github.io/edit/site/src/content/blog';
