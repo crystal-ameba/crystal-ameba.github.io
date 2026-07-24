61f5e20603c13a85c2d644c86d1a4a4620c3796f
Sijawusz Pur Rahnama <sija@sija.pl>
2026-07-25 00:46:37 +0200

    Cleanup `CONTRIBUTORS_URL` const

--- src/consts.ts
+++ src/consts.ts
@@ -8,7 +8,7 @@ export const BLOG_DESCRIPTION = 'Release announcements and articles about the Am
 export const GITHUB_URL = 'https://github.com/crystal-ameba/ameba';
 export const GITHUB_RELEASES_URL = `${GITHUB_URL}/releases`;
 export const CONTRIBUTING_URL = `${GITHUB_URL}/blob/master/CONTRIBUTING.md`;
-export const CONTRIBUTORS_URL = 'https://github.com/crystal-ameba/ameba/graphs/contributors?all=1';
+export const CONTRIBUTORS_URL = `${GITHUB_URL}/graphs/contributors?all=1`;
 export const DOCS_URL = 'https://crystal-ameba.org/ameba/';
 export const RULES_REFERENCE_URL = DOCS_URL; // TODO
 
